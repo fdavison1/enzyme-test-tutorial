60a785bf85268fc3b6aef77183dd69a78246db7e
fdavison1 <fred.noreply@gmail.com>
2019-12-04 12:00:04 -0700

    initial set up

---
+++ src/components/Lists.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function List(props){
+    const { items } = props
+    if (!items.length){
+        //no items on list, render empty message
+        return <span className='empty-message'> No items in list</span>
+    }
+    return (
+        <ul className='list-items'>
+            {items.map(item => <li key={item} className='item'>{item}</li>)}
+        </ul>
+    )
+}
+
+List.propTypes = {
+    items: PropTypes.array
+}
+
+List.defaultProps = {
+    items: []
+}
+
+export default List
