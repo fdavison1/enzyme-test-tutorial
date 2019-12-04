9309f976dd060d3f251168b0d158af23cd2c3605
fdavison1 <fred.noreply@gmail.com>
2019-12-04 12:16:23 -0700

    shallow tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Lists from './components/Lists'
+import List from './components/List'
 
 function App() {
   return (
     <div className="App">
-      <Lists/>
+      <List/>
     </div>
   );
 }
